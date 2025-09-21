3664187f5fb6f4b76930d725ad66259c4d2de24f
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-09-21 10:37:35 +0200

    fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
 # clone this repository
-if [ ! -e $HOME/dev-onboarding ]; then
+if [ ! -e $HOME/.dotfiles ]; then
   echo "cloning the repository into your machine"
   git clone https://github.com/Amheklerior/dotfiles-repo $HOME/.dotfiles
 else
   echo "repo already present, making sure it's up to date"
-  cd $HOME/dev-onboarding && git pull || exit 1
+  cd $HOME/.dotfiles && git pull || exit 1
 fi
 
 # run the bootstrap script

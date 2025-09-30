ef60263e74e907626750099c3a8a4501961142ce
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-09-30 09:23:43 +0200

    simplify setup scripts avoiding unnecessary use of temporary support files

--- scripts/setup_github_account.sh
+++ scripts/setup_github_account.sh
@@ -10,16 +10,11 @@ fi
 echo "$LOG_PREFIX setting up github cli"
 
 # decrypt and copy the github ACCESS_TOKEN to the clipboard
-cp $DOTFILES_REPO/private/github.token $XDG_DATA_HOME/gh-login-token
 echo "$LOG_PREFIX Please enter the decryption password for copying the github login token"
-ansible-vault decrypt $XDG_DATA_HOME/gh-login-token
-cat $XDG_DATA_HOME/gh-login-token | pbcopy
+ansible-vault decrypt $DOTFILES_REPO/secrets/github-access-token --output - | pbcopy
 echo "$LOG_PREFIX token successfully copeid on your clipboard! Paste it during the installation process..."
 
 # login to the github account
 gh auth login
-
-# cleanup temporary files
-rm $XDG_DATA_HOME/gh-login-token
 
 echo "$LOG_PREFIX github cli setup complete!"

d40ede09031047ecd831835329e88445949b3653
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-12-17 19:10:09 +0100

    integrate mason-lspconfig plugin to autoinstall and autoenable lsp servers

--- dotfiles/.config/nvim/lua/amheklerior/lsp.lua
+++
@@ -1,5 +0,0 @@
--- enable language servers for the Neovim LSP client
---
--- INFO: default configurations are provided by the `neovim/nvim-lsconfig` plugin
--- so, no need to define them by hand every time you want to enable a new LSP server.
-vim.lsp.enable("lua_ls");
--- dotfiles/.config/nvim/lua/amheklerior/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/amheklerior/plugins/lsp.lua
@@ -1,13 +1,22 @@
-
 return {
-  -- package manager for Neovim, to install and manage external editor tools
-  --  like LSP servers, DAP servers, linters, and formatters.
-  --
-  -- INFO: to check the current status of installed tools and/or manually install others,
-  -- you can run `:Mason`
-  { "mason-org/mason.nvim", opts = {} },
+  -- automatically install and enable LSP servers,
+  "mason-org/mason-lspconfig.nvim",
+  dependencies = {
+    -- package manager for Neovim, to install and manage external editor tools
+    --  like LSP servers, DAP servers, linters, and formatters.
+    { "mason-org/mason.nvim", opts = {} },
 
-  -- a collection of community curated LSP servers configurations
-  -- NOTE: overrides are located into the `/after/lsp` directory
-  "neovim/nvim-lspconfig",
+    -- a collection of community curated LSP servers configurations
+    -- NOTE: overrides are located into the `/after/lsp` directory
+    "neovim/nvim-lspconfig",
+  },
+  opts = {
+    -- automatically install these LSP servers
+    -- NOTE: it understands server names as defined in nvim-lspconfig and figure out
+    -- which Mason package needs to install
+    ensure_installed = { 'lua_ls' },
+
+    -- auto enable them (no need to call `vim.lsp.enable(...)` manually)
+    automatic_enable = true,
+  },
 }

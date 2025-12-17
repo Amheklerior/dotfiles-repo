4cdb4abe22da80db47e4d5cdd06fbc4c6c3f674a
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-12-17 17:48:06 +0100

    autoload configuration from the /nvim/lsp dir

--- dotfiles/.config/nvim/lua/amheklerior/lsp.lua
+++ dotfiles/.config/nvim/lua/amheklerior/lsp.lua
@@ -1,23 +1,4 @@
-
--- configure Lua LSP server
-vim.lsp.config("lua-server", {
-  -- the executable installed through Mason,
-  -- to let vim LSP client know how to run the server
-  cmd = { 'lua-language-server' },
-
-  -- attach this LSP server when opening a lua file
-  filetypes = { 'lua' },
-  root_markers = {
-    '.luarc.json',
-    '.luarc.jsonc',
-    '.luacheckrc',
-    '.stylua.toml',
-    'stylua.toml',
-    'selene.toml',
-    'selene.yml',
-    '.git',
-  },
-})
-
--- enable the LSP
-vim.lsp.enable("lua-server");
+-- enable the Lua language server
+-- NOTE: its configurations are automatically taken from the
+--  `nvim/lsp/lua_ls.lua` file 
+vim.lsp.enable("lua_ls");

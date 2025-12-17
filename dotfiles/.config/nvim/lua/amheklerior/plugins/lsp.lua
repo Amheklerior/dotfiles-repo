31cb0029ec835cfbbff213260395f84ac8c218dc
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-12-17 16:19:05 +0100

    integrate Mason as nvim package manager

---
+++ dotfiles/.config/nvim/lua/amheklerior/plugins/lsp.lua
@@ -0,0 +1,7 @@
+
+-- package manager for Neovim, to install and manage external editor tools
+--  like LSP servers, DAP servers, linters, and formatters.
+--
+-- INFO: to check the current status of installed tools and/or manually install others,
+-- you can run `:Mason`
+return { "mason-org/mason.nvim", opts = {} }

70153fe0f21f0b753c61e0bb0b2e93d5bc62c816
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-12-17 18:03:55 +0100

    integrate nvim-lspconfig to avoid manually defining server configs

--- dotfiles/.config/nvim/lua/amheklerior/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/amheklerior/plugins/lsp.lua
@@ -1,7 +1,12 @@
 
--- package manager for Neovim, to install and manage external editor tools
---  like LSP servers, DAP servers, linters, and formatters.
---
--- INFO: to check the current status of installed tools and/or manually install others,
--- you can run `:Mason`
-return { "mason-org/mason.nvim", opts = {} }
+return {
+  -- package manager for Neovim, to install and manage external editor tools
+  --  like LSP servers, DAP servers, linters, and formatters.
+  --
+  -- INFO: to check the current status of installed tools and/or manually install others,
+  -- you can run `:Mason`
+  { "mason-org/mason.nvim", opts = {} },
+
+  -- a collection of community curated LSP servers configurations
+  "neovim/nvim-lspconfig",
+}

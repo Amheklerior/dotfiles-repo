6cab041b52f27e3a9ef0dfb6a30500086524b943
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-12-18 09:02:16 +0100

    cleanup

--- dotfiles/.config/nvim/lua/amheklerior/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/amheklerior/plugins/treesitter.lua
@@ -18,20 +18,20 @@ return {
     enabled = true,
     -- make sure all parsers are updated to the latest version when updating the plugin
     build = ':TSUpdate',
-    -- Sets main module to use for opts
+    -- sets main module to use for opts
     main = 'nvim-treesitter.configs',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       -- list of language parsers. Can choose 'all' or 'maintained', instead of listing specific parsers
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall missing language parsers when entering a buffer
+      -- autoinstall missing language parsers when entering a buffer
       auto_install = true,
 
       -- ↓↓↓ treesitter applications ↓↓↓
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
+        -- some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  if you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
@@ -40,10 +40,10 @@ return {
         disable = { 'ruby' }
       },
     },
-    -- other interesting applications to explore:
+
+    -- TODO: other interesting applications to explore:
     -- Treesitter + textobjects   -> https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     -- Show your current context  -> https://github.com/nvim-treesitter/nvim-treesitter-context
-    -- View and navigate the AST  -> https://github.com/nvim-treesitter/playground
     -- Rainbow brackets           -> https://github.com/p00f/nvim-ts-rainbow
     -- Code folding               -> https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#folding
     -- Incremental selection      -> :help nvim-treesitter-incremental-selection-mod`

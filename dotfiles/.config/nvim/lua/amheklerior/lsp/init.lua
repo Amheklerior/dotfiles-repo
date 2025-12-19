656f83bde2624fcbecd34d1b088b2f3ead2b5ab6
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-12-19 17:32:14 +0100

    split LSP configs into multiple files

---
+++ dotfiles/.config/nvim/lua/amheklerior/lsp/init.lua
@@ -0,0 +1,25 @@
+local lsp_highlight = require "amheklerior/lsp/highlight"
+local lsp_navigation_keymaps = require "amheklerior/lsp/keymaps"
+local lsp_diagnostics = require "amheklerior/lsp/diagnostics"
+
+lsp_diagnostics.setup()
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+  callback = function(event)
+    vim.notify "setting up LSP..."
+
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+
+    -- toggle inlay hints in your code (sometimes they get annoying)
+    map('<leader>th', function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+    end, '[T]oggle Inlay [H]ints')
+
+    lsp_navigation_keymaps.setup(event, map)
+    lsp_highlight.setup(event)
+  end
+})
---
+++ dotfiles/.config/nvim/lua/amheklerior/lsp/keymaps.lua
@@ -0,0 +1,8 @@
+
+local M = {}
+
+M.setup = function (evt, map)
+  -- TODO: add my custom keymaps
+end
+
+return M

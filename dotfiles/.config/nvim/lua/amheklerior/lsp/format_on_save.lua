35d10ed7157845753b86a9a9d4fc34a65c025499
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-12-22 21:14:58 +0100

    integrate lsp autoformatting on save

---
+++ dotfiles/.config/nvim/lua/amheklerior/lsp/format_on_save.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+M.setup = function(evt)
+  local client = vim.lsp.get_client_by_id(evt.data.client_id)
+
+  -- do nothing if the LSP client is not found or it doesn't support formatting
+  if not client then
+    vim.notify "format on save: no lsp client found!"
+    return
+  end
+  ---@diagnostic disable-next-line: missing-parameter, param-type-mismatch
+  if not client:supports_method('textDocument/formatting') then
+    vim.notify "format on save: lsp doesn't support formatting!"
+    return
+  end
+
+  -- setup an autocommand to format the current buffer on save
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    buffer = evt.buf,
+    callback = function()
+      vim.lsp.buf.format { bufnr = evt.buf, id = client.id }
+    end,
+  })
+end
+
+return M

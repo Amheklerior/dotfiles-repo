f8da9a30f4d6688a8e1840f9d03cab673403b266
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2025-12-22 21:14:58 +0100

    integrate lsp autoformatting on save

--- dotfiles/.config/nvim/lua/amheklerior/lsp/init.lua
+++ dotfiles/.config/nvim/lua/amheklerior/lsp/init.lua
@@ -1,6 +1,7 @@
 local lsp_highlight = require "amheklerior/lsp/highlight"
 local lsp_keymaps = require "amheklerior/lsp/keymaps"
 local lsp_diagnostics = require "amheklerior/lsp/diagnostics"
+local format_on_save = require "amheklerior/lsp/format_on_save"
 
 lsp_diagnostics.setup()
 
@@ -9,10 +10,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
     local map = function(keys, func, desc, mode)
       mode = mode or 'n'
-      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+      vim.keymap.set(mode, keys, func, {
+        buffer = event.buf,
+        desc = 'LSP: ' .. desc
+      })
     end
 
     lsp_keymaps.setup(event, map)
     lsp_highlight.setup(event, map)
+    format_on_save.setup(event)
   end
 })

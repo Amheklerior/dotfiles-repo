9e2967edb8a5abe5c450bfd11be01cb1c00d2a1f
Amheklerior <49590425+Amheklerior@users.noreply.github.com>
2026-02-13 14:42:15 +0100

    improve diagnostic configs

--- dotfiles/.config/nvim/lua/amheklerior/lsp/diagnostics.lua
+++ dotfiles/.config/nvim/lua/amheklerior/lsp/diagnostics.lua
@@ -5,11 +5,27 @@ local severity = vim.diagnostic.severity
 -- for info see `:help vim.diagnostic.Opts`
 M.setup = function()
   vim.diagnostic.config {
-    severity_sort = true,
-    float = { border = "rounded", source = "if_many" },
+    -- don't update messages with every keystroke (wait till the end of the edit)
+    update_in_insert = false,
+
+    -- show messages at the end of the line instead of underneath it
+    virtual_text = true,
+    virtual_lines = false,
+
+    -- sort messages by severity
+    seaverity_sort = true,
+
+    -- underline error messages
     underline = { severity = severity.ERROR },
 
-    -- use simbols instead of letters if a nerf font is available
+    -- don't auto open the floating window when jumping with `[d` and `]d`
+    -- NOTE: you can open up the floating window with `:lua vim.diagnostic.open_float()`
+    jump = { float = false },
+
+    -- customize floating window
+    float = { border = "rounded", source = "if_many" },
+
+    -- use simbols instead of letters if a nerd font is available
     signs = vim.g.have_nerd_font and {
       text = {
         [severity.ERROR] = "✖︎ ",
@@ -18,20 +34,6 @@ M.setup = function()
         [severity.HINT] = "󰌶 ",
       },
     } or {},
-
-    virtual_text = {
-      source = "if_many",
-      spacing = 2,
-      format = function(diagnostic)
-        local diagnostic_message = {
-          [severity.ERROR] = diagnostic.message,
-          [severity.WARN] = diagnostic.message,
-          [severity.INFO] = diagnostic.message,
-          [severity.HINT] = diagnostic.message,
-        }
-        return diagnostic_message[diagnostic.severity]
-      end,
-    },
   }
 end
 
